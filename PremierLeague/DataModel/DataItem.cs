d6fa161c3f848715d6194d1bea55f5e791d2c313
Harri Lehtola <peruukki@hotmail.com>
2012-11-27 22:58:39 +0200

    New ItemTemplate for the teams

--- PremierLeague/DataModel/DataItem.cs
+++ PremierLeague/DataModel/DataItem.cs
@@ -10,5 +10,7 @@
 
         public string Name { get; private set; }
         public int FFTPrediction { get; private set; }
+
+        public string FFTPredictionText { get { return "FFT: " + FFTPrediction; } }
     }
 }
