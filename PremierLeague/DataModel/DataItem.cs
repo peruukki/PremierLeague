2332222a020fbf16dbce8311fc7af073a08a2039
Harri Lehtola <peruukki@hotmail.com>
2012-12-01 23:12:32 +0200

    Update current team positions in a more convenient way

--- PremierLeague/DataModel/DataItem.cs
+++ PremierLeague/DataModel/DataItem.cs
@@ -2,15 +2,14 @@
 {
     public class DataItem
     {
-        public DataItem(string name, int currentPosition, int fftPrediction)
+        public DataItem(string name, int fftPrediction)
         {
             Name = name;
-            CurrentPosition = currentPosition;
             FFTPrediction = fftPrediction;
         }
 
         public string Name { get; private set; }
-        public int CurrentPosition { get; private set; }
+        public int CurrentPosition { get; set; }
         public int FFTPrediction { get; private set; }
         public int PositionDifference { get { return FFTPrediction - CurrentPosition; } }
 
